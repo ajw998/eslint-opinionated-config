d402829091b56dda68437b5ad16e355559622965
Andrew Wong <andrewwong998@gmail.com>
2020-04-08 22:58:53 +0100

    Add rules comments and documentation link

--- rules/style.js
+++ rules/style.js
@@ -11,13 +11,40 @@ module.exports = {
     ...characters,
     ...comment,
     ...layout,
+
+    // Ensure consistent linbreak style
+    // https://eslint.org/docs/rules/linebreak-style
     'linebreak-style': 'off',
-    'max-lines': ['warn', {max: 300}],
+
+    // Prefer max 300 lines of code in files
+    // https://eslint.org/docs/rules/max-lines
+    'max-lines': ['warn', {max: 300, skipComments: true}],
+
+    // Prefer max 4 paramters in functions
+    // https://eslint.org/docs/rules/max-params
     'max-params': ['warn', 4],
+
+    // Always allow ternary
+    // https://eslint.org/docs/rules/no-ternary
     'no-ternary': 'off',
+
+    // Blanket ban on trailing white spaces
+    // https://eslint.org/docs/rules/no-trailing-spaces
     'no-trailing-spaces': 'error',
+
+    // Prefer using object spread over `Object.assign()`
+    // https://eslint.org/docs/rules/prefer-object-spread
     'prefer-object-spread': 'warn',
+
+    // Prefer object keys sorting
+    // https://eslint.org/docs/rules/sort-keys
     'sort-keys': 'warn',
-    'sort-vars': 'warn', // Documentation needed
+
+    // Prefer variable sorting
+    // https://eslint.org/docs/rules/sort-vars
+    'sort-vars': 'warn',
+
+    // Disallow yoda conditions
+    // https://eslint.org/docs/rules/yoda
     yoda: ['error', 'never'],
 };
