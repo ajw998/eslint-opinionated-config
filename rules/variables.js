c03e0c16017e66f00b74d8c21940d052c33bcb32
Andrew Wong <andrewwong998@gmail.com>
2020-05-06 23:52:05 +0100

    Add rules documentation

--- rules/variables.js
+++ rules/variables.js
@@ -2,12 +2,36 @@
  * Variable rules
  */
 module.exports = {
+
+    // Prefer initialisation in variable declarations
+    // https://eslint.org/docs/rules/init-declarations
     'init-declarations': 'warn',
+
+    // Disallow deleting variables
+    // https://eslint.org/docs/rules/no-delete-var
     'no-delete-var': 'error',
+
+    // Disallow variables from shadowing variables declared in the outer scope
+    // https://eslint.org/docs/rules/no-shadow
     'no-shadow': 'error',
+
+    // Disallow shadowing of restricted names
+    // https://eslint.org/docs/rules/no-shadow-restricted-names
     'no-shadow-restricted-names': 'error',
+
+    // Disallow undeclared variables
+    // https://eslint.org/docs/rules/no-undef
     'no-undef': 'error',
+
+    // Disallow initialisation to undefined
+    // https://eslint.org/docs/rules/no-undef-init
     'no-undef-init': 'error',
+
+    // Disallow use of `undefined`
+    // https://eslint.org/docs/rules/no-undefined
     'no-undefined': 'warn',
+
+    // Disallow early use
+    // https://eslint.org/docs/rules/no-use-before-define
     'no-use-before-define': 'error',
 };
