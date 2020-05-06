9c194c998ce6df81f9e4809e1bf06f8f9432f7de
Andrew Wong <andrewwong998@gmail.com>
2020-05-06 23:56:34 +0100

    Add rules documentation

--- rules/es6.js
+++ rules/es6.js
@@ -7,13 +7,39 @@ module.exports = {
     // Disallow `super()` call in non-derived classes
     // https://eslint.org/docs/rules/constructor-super
     'constructor-super': 'error',
+
+    // Disallow modifying variables of class declarations
+    // https://eslint.org/docs/rules/no-class-assign
     'no-class-assign': 'error',
+
+    // Disallow symbol constructor
+    // https://eslint.org/docs/rules/no-new-symbol
     'no-new-symbol': 'error',
+
+    // Disallow `this` before calling super() in constructors
+    // https://eslint.org/docs/rules/no-this-before-super
     'no-this-before-super': 'error',
+
+    // Disallow unnecessary computed property keys in objects and classes
+    // https://eslint.org/docs/rules/no-useless-computed-key
     'no-useless-computed-key': 'error',
+
+    // Disallow renaming import, export and destructured assignments
+    // to the same name
+    // https://eslint.org/docs/rules/no-useless-rename
     'no-useless-rename': 'error',
+
+    // Use `const` for variable declarations
+    // https://eslint.org/docs/rules/prefer-const
     'prefer-const': 'error',
+
+    // Disallow parseInt() and Number.parseInt() in favor of binary, octal, and
+    // hexadecimal literals
+    // https://eslint.org/docs/rules/prefer-numeric-literals
     'prefer-numeric-literals': 'warn',
+
+    // Require use of template literals instead of string concatenation
+    // https://eslint.org/docs/rules/prefer-template
     'prefer-template': 'error',
 
     // Enforce consistent spacing in rest spread operators
