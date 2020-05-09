c8e9e1dd131f1e48c7d4600130414b55d59c51a3
Andrew Wong <andrewwong998@gmail.com>
2020-05-09 18:37:17 +0100

    Add rules documentation

--- rules/layout.js
+++ rules/layout.js
@@ -1,6 +1,18 @@
 module.exports = {
+
+    // Disallow spaces inside arrays
+    // https://eslint.org/docs/rules/array-bracket-spacing
     'array-bracket-spacing': ['error', 'never', {arraysInArrays: true}],
+
+    // Enforce newline before dot notation
+    // https://eslint.org/docs/rules/dot-location
     'dot-location': ['error', 'property'],
+
+    // Enforce trailing newline in end of file
+    // https://eslint.org/docs/rules/eol-last
     'eol-last': ['error', 'always'],
+
+    // Enforce 4-spaces indentation
+    // https://eslint.org/docs/rules/indent
     indent: ['error', 4],
 };
