eef60db96b88624353c6e3e24b3beb423789a322
Andrew Wong <andrewwong998@gmail.com>
2020-05-16 21:54:39 +0100

    Add rules documentation

--- rules/characters.js
+++ rules/characters.js
@@ -1,7 +1,15 @@
 module.exports = {
-    'no-tabs': 'error',
+
+    // Disallow trailing whitespace
+    // https://eslint.org/docs/rules/no-trailing-spaces
     'no-trailing-spaces': 'error',
+
+    // Require quotes around objects only when necessary
+    // https://eslint.org/docs/rules/quote-props
     'quote-props': ['warn', 'as-needed'],
+
+    // Enforce single quotes
+    // https://eslint.org/docs/rules/quotes
     quotes: [
         'error',
         'single',
@@ -10,7 +18,16 @@ module.exports = {
             avoidEscape: true
         }
     ],
+
+    // Require semicolons
+    // https://eslint.org/docs/rules/semi
     semi: ['error', 'always', { omitLastInOneLineBlock: true }],
+
+    // Enforce location of semicolons
+    // https://eslint.org/docs/rules/semi-style
     'semi-style': ['error', 'last'],
+
+    // Require regex literals to be wrapped
+    // https://eslint.org/docs/rules/wrap-regex
     'wrap-regex': 'error',
 };
