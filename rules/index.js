cc5cfa546bc18a02ea1b92a40dafca42e598058b
Andrew Wong <andrewwong998@gmail.com>
2020-07-23 10:52:25 +0100

    Remove node engines requirement

--- rules/index.js
+++ rules/index.js
@@ -2,7 +2,6 @@ const base = require('./base');
 const bestPractices = require('./best-practices');
 const es6 = require('./es6');
 const jest = require('./jest');
-const node = require('./node');
 const style = require('./style');
 const unicorn = require ('./unicorn');
 const variables = require('./variables');
@@ -11,7 +10,6 @@ module.exports = {
     env: {
         browser: true,
         jest: true,
-        node: true,
     },
     extends: [
         'plugin:react/recommended',
@@ -27,7 +25,6 @@ module.exports = {
     plugins: [
         'jest',
         'no-use-extend-native',
-        'node',
         'react',
         'unicorn',
     ],
@@ -36,7 +33,6 @@ module.exports = {
         ...bestPractices,
         ...es6,
         ...jest,
-        ...node,
         ...style,
         ...unicorn,
         ...variables,
--- rules/jest.js
+++ rules/jest.js
@@ -2,6 +2,9 @@
  * Jest rules
  */
 module.exports = {
+    // Enforce assertion to be made in test body
     'jest/expect-expect': 'error',
+
+    // Enforce valid expect() usage
     'jest/valid-expect': 'error',
 };
